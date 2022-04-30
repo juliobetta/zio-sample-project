326bbe6f89aa1825bd7314085e96041b5e4e6101
Julio Betta <juliobetta@gmail.com>
2022-04-30 00:01:24 -0300

    remove namespace

--- charts/scripts/install.sh
+++ charts/scripts/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # When you want to test the template rendering, but not actually install anything, you can use
 
-helm install zio-app --debug ./charts/zio-app -n zio
+helm install zio-app --debug ./charts/zio-app
--- charts/scripts/uninstall.sh
+++ charts/scripts/uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # When you want to test the template rendering, but not actually install anything, you can use
 
-kubectl delete deployment zio-chart -n zio
+kubectl delete deployment zio-app-chart
 sleep 5
-helm uninstall zio-app --debug -n zio
+helm uninstall zio-app --debug
